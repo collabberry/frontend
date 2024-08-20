cf6e5613628fe5a338744cb8e32857413ea909b3
migrenaa <mimonova13@gmail.com>
2024-08-20 17:04:58 +0300

    initial setup theme

---
+++ src/mock/data/authData.ts
@@ -0,0 +1,11 @@
+export const signInUserData = [
+    {
+        id: '21',
+        avatar: '/img/avatars/thumb-1.jpg',
+        userName: 'Milena',
+        email: 'milena@collabberry.xyz',
+        authority: ['admin', 'user'],
+        password: '123Qwe',
+        accountUserName: 'admin',
+    },
+]
