c3a5961e9da0ba9f097390d7291206900922b396
pryingmoth <apryingmoth@gmail.com>
2025-01-16 15:08:49 +0200

    show tx has on deploy tp contract during sign-up flow

--- src/components/collabberry/custom-components/TransactionSuccessDialog.tsx
+++ src/components/collabberry/custom-components/TransactionSuccessDialog.tsx
@@ -13,7 +13,7 @@ interface SuccessDialogProps {
     handleDialogClose: () => void;
 }
 
-const shortenTxHash = (hash: string, txBlockExplorer: string, start = 6, end = 4,) => {
+export const shortenTxHash = (hash: string, txBlockExplorer: string, start = 6, end = 4,) => {
     if (!hash) return ''
     return `${txBlockExplorer}${hash.slice(0, start)}...${hash.slice(-end)}`
 }
@@ -32,7 +32,7 @@ const SuccessDialog: React.FC<SuccessDialogProps> = ({
         <Dialog isOpen={dialogVisible} onClose={handleDialogClose} shouldCloseOnOverlayClick>
             <div className=" flex flex-col items-center justify-center p-2 min-h-[200px]">
                 <div className="flex flex-col items-center">
-                    <h2 className="text-xl font-bold mb-3 mt-3 text-center">
+                    <h2 className="text-xl font-bold mb-4 mt-3 text-center">
                         {dialogMessage}
                     </h2>
                     <div className="mb-2 text-center text-md flex flex-col items-center">
